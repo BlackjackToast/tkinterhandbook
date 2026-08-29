9c6264a1889f980512be0d6d5678cc17e19c3676
BlackjackToastington <simplytoasnificent@gmail.com>
2026-08-28 21:09:07 -0400

    Add files via upload

---
+++ tutforpyforbeg.py
@@ -0,0 +1,35 @@
+#Print or known as print() is a really simple thing to understand
+#print()simply prints something to the terminal/output
+
+#example
+print("Hello World!")
+# This will print a message saying Hello World! remember to add quotations to it!
+
+
+#functions with Print
+#Functions are simply a block of code that performs a task that can be recalled
+#Example
+examplefunc = "Hi!"
+#you can name functions anything
+#then you can print with it!
+print(examplefunc)
+#No need for quotations!
+#this should be all you need for printing for now.
+
+
+#if statements
+#if statments are basically if something happens then this happens
+
+#example
+
+#if examplefunc == "Hello!": 
+    #print("Goodbye!")
+
+#Make sure it is exactly like your function one typo can cause errors
+#then there is else statements!
+
+if examplefunc == "Hello!":
+    print("Goodbye!")
+else:
+    print("Hello!")
+    
